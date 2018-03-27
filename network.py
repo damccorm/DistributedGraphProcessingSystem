6d35ed52a4c2359834158924e53cb994ff418cf6
Danny McCormick <dannymccormick3@gmail.com>
2018-03-27 16:11:07 -0500

    Updating formatting of loader

--- network.py
+++ network.py
@@ -5,10 +5,10 @@ class Network:
 
 	def __init__(self, ip_address, master_ip):
 		self.context = zmq.Context()
-                # Socket to receive all communications not from the master
-                self.not_master_sub_socket = self.context.socket(zmq.SUB)
-                self.not_master_sub_socket.bind("tcp://"+ip_address+":5555")
-                self.not_master_sub_socket.setsockopt_string(zmq.SUBSCRIBE, "worker".decode("ascii"))
+		# Socket to receive all communications not from the master
+		self.not_master_sub_socket = self.context.socket(zmq.SUB)
+		self.not_master_sub_socket.bind("tcp://"+ip_address+":5555")
+		self.not_master_sub_socket.setsockopt_string(zmq.SUBSCRIBE, "worker".decode("ascii"))
 		self.is_master = True
 
 		#Sockets to send/receive all communications from the master
@@ -17,9 +17,9 @@ class Network:
 		if master_ip is not None:
 			self.is_master = False
 			self.master_pub_socket = self.context.socket(zmq.SUB)
-    		        self.master_pub_socket.bind("tcp://"+master_ip+":5555")
+			self.master_pub_socket.bind("tcp://"+master_ip+":5555")
 			self.master_sub_socket = self.context.socket(zmq.SUB)
-    		        self.master_sub_socket.bind("tcp://"+master_ip+":5556")
+			self.master_sub_socket.bind("tcp://"+master_ip+":5556")
 			self.master_sub_socket.setsockopt_string(zmq.SUBSCRIBE, "master".decode("ascii"))
 		else:
 			# Make sure that master can listen for loading message as well as messages from workers
@@ -49,22 +49,20 @@ class Network:
 		if self.is_master:
 			print "ERROR, tried to wait for master from master"
 			return
-		msg =  {"message_type": message_type,
-				"contents": message_contents,
-				"vertex_number": vertex_number}
-                self.master_sub_socket.send_string("%s %s" % ("worker", json.dumps(msg, separators=(",",":"))))
+		msg =  {"message_type": message_type, "contents": message_contents, "vertex_number": vertex_number}
+		self.master_sub_socket.send_string("%s %s" % ("worker", json.dumps(msg, separators=(",",":"))))
 
         def add_edge(self, outgoing_vertex, outgoing_ip):
-                if outgoing_ip not in self.ip_map:
-    		        pub_socket = self.context.socket(zmq.PUB)
-    		        if self.is_master:
-    			        pub_socket.bind("tcp://"+outgoing_ip+":5556")
-    		        else:
-    			        pub_socket.bind("tcp://"+outgoing_ip+":5555")
-    		        self.edges.append(pub_socket)
-    		        self.ip_map[outgoing_ip] = len(self.edges) - 1
-		if outgoing_vertex is not None and outgoing_vertex not in self.edge_map:
-			self.edge_map[outgoing_vertex] = self.ip_map[outgoing_ip]
+        	if outgoing_ip not in self.ip_map:
+        		pub_socket = self.context.socket(zmq.PUB)
+        		if self.is_master:
+        			pub_socket.bind("tcp://"+outgoing_ip+":5556")
+        		else:
+        			pub_socket.bind("tcp://"+outgoing_ip+":5555")
+    			self.edges.append(pub_socket)
+    			self.ip_map[outgoing_ip] = len(self.edges) - 1
+			if outgoing_vertex is not None and outgoing_vertex not in self.edge_map:
+				self.edge_map[outgoing_vertex] = self.ip_map[outgoing_ip]
 
 	def send_to_worker(self, receiving_vertex, sending_vertex, message_contents, message_type):
 		msg =  {"message_type": message_type,
