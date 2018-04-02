87d4e6deba5e31bd4ee358d533d5a84bc8395837
Danny McCormick <dannymccormick3@gmail.com>
2018-04-02 10:37:56 -0500

    Working on imports

--- shortest_path/shortest_path_worker.py
+++ shortest_path/shortest_path_worker.py
@@ -25,13 +25,13 @@ def output_function(vertex):
 
 
 if __name__ == '__main__':
-	if __package__ is None:
-        import sys
-        from os import path
-        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-        from Distributed-Graph-Processing-System.worker import Worker
-    else:
-        from ..Distributed-Graph-Processing-System.worker import Worker
+        if __package__ is None:
+                import sys
+                from os import path
+                sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+                from Distributed-graph-Processing-System.worker import Worker
+        else:
+                from ..Distributed-graph-Processing-System.worker import Worker
 	master_ip_address = None
 	own_ip_address = "127.0.0.2"
 	if len(sys.argv) > 1:
