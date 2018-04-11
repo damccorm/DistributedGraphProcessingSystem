c2168e2ef6f8a4b1464787f9273ba0a8b6ee95e0
Danny McCormick <dannymccormick3@gmail.com>
2018-04-10 23:06:55 -0500

    Adding first attempt at topological sort

---
+++ topological_sort/tsMaster.py
@@ -0,0 +1,18 @@
+"""
+Master node with purpose of computing a topoplogical sort.
+Each vertex should initially have 0 as it's value.
+"""
+
+
+if __name__ == '__main__':
+	if __package__ is None:
+		import sys
+		from os import path
+		sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+		from master import Master
+	else:
+		from ..master import Master
+	ip_address = "127.0.0.1"
+	if len(sys.argv) > 1:
+		ip_address = sys.argv[1]
+	master = Master(ip_address)
---
+++ topological_sort/tsWorker.py
@@ -0,0 +1,51 @@
+"""
+Worker node with purpose of computing a topoplogical sort.
+Each vertex should initially have 0 as it's value.
+"""
+
+def compute(vertex, input_value, incoming_messages, send_message_to_vertex):
+	if int(vertex.vertex_value) == 0:
+		vertex.vertex_value = [1, -1]
+	elif isinstance(vertex.vertex_value, list):
+		# Still needs to be placed
+		if len(incoming_messages) == 0:
+			vertex.active = False
+			vertex.vertex_value = vertex.vertex_value[0]
+		else:
+			if len(incoming_messages) == vertex.vertex_value[1]:
+				# If it got the same number of messages last time, mark inactive since progress may not have been made
+				vertex.active = False
+			else:
+				vertex.active = True
+			vertex.vertex_value[0] = vertex.vertex_value[0] + 1
+			vertex.vertex_value[1] = len(incoming_messages)
+			for v in vertex.outgoing_edges:
+				send_message_to_vertex(vertex, v, "")
+	return vertex, "TODO"
+
+def output_function(vertex):
+	if isinstance(vertex.vertex_value, list):
+		print "Vertex", vertex.vertex_number, "could not be sorted, there must be a loop"
+	else:
+		print "Vertex", vertex.vertex_number, "was sorted into position", vertex.vertex_value
+
+
+if __name__ == '__main__':
+	if __package__ is None:
+		import sys
+		from os import path
+		sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+		from worker import Worker
+	else:
+		from ..worker import Worker
+	master_ip_address = None
+	own_ip_address = "127.0.0.2"
+	if len(sys.argv) > 1:
+		master_ip_address = sys.argv[1]
+		if len(sys.argv) > 2:
+			own_ip_address = sys.argv[2]
+		compute_lambda = lambda vertex, input_value, incoming_messages, send_message_to_vertex: compute(vertex, input_value, incoming_messages, send_message_to_vertex)
+		output_lambda = lambda vertex: output_function(vertex)
+		worker = Worker(master_ip_address, own_ip_address, compute_lambda, output_lambda)
+	else:
+		print "ERROR, must add the address of the master as an argument"
